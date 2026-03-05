b4e630e627c24985de88eb027c3a517c856e0cc9
Caio Prieto <caiohideki23@gmail.com>
2026-03-05 14:37:47 -0300

    Feat: Incluir botão de copiar

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Landing Apex Safety carregada com sucesso.");
 });
+
+function copyEmail() {
+  const email = document.getElementById("email-text").innerText;
+
+  navigator.clipboard.writeText(email);
+
+  const feedback = document.getElementById("copy-feedback");
+  feedback.style.opacity = "1";
+
+  setTimeout(() => {
+    feedback.style.opacity = "0";
+  }, 2000);
+}

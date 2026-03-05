be7bcbe8d8681e96f49b7f47fcc097710b2a3363
Caio Prieto <caiohideki23@gmail.com>
2026-03-05 14:43:41 -0300

    Ajuste js

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function copyEmail() {
   const email = document.getElementById("email-text").innerText;
-
-  navigator.clipboard.writeText(email);
-
   const feedback = document.getElementById("copy-feedback");
+
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(email);
+  } else {
+    const textarea = document.createElement("textarea");
+    textarea.value = email;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+
   feedback.style.opacity = "1";
 
   setTimeout(() => {
